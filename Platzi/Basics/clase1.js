955a58a2cde2d96fffedac03b6cab1197e7d187a
Aldhair Vera <aldhairvera15@gmail.com>
2019-09-04 01:54:56 -0500

    class 1-class 2 Platzi JS Fundamentals

---
+++ Platzi/Basics/clase1.js
@@ -0,0 +1,11 @@
+const div=document.querySelector('div');
+
+//Class 1
+var name='Aldhair',lastname='Vera Camacho';
+var fullname=name+" "+lastname;
+
+let age='25';
+console.log(fullname);
+
+//Class 2
+div.innerText=fullname + '\n' +"Age: "+age;
